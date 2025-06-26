2a6dac3518c03831bebf409cb93ae95eb5fef1d7
ARTPrime <primaltorub@gmail.com>
2025-06-25 20:08:52 -0600

    feat: Adds form validation

--- scripts/test_script.js
+++ scripts/test_script.js
@@ -1,11 +1,11 @@
 // 1. Tipos de variables
 let userName;
-userName = "Andres";
+userName = 'Andres';
 console.log(userName);
 userName = 10;
 console.log(userName);
 
-const userType = "admin";
+const userType = 'admin';
 console.log(userType);
 
 // 2. Tipos de datos (typeof)
@@ -18,33 +18,33 @@ let userHasPermission = true;
 console.log(typeof userHasPermission);
 
 let user = {
-  name: "Andres",
-  lastName: "Rubio",
+    name: 'Andres',
+    lastName: 'Rubio',
 };
 console.log(typeof user);
 // 2.1. Cadenas literales
-let userLastName = "Rubio";
-let userLastName2 = "Torres";
+let userLastName = 'Rubio';
+let userLastName2 = 'Torres';
 
-console.log(userLastName + " " + userLastName2);
+console.log(userLastName + ' ' + userLastName2);
 console.log(`${userLastName}-${userLastName2}.`);
 
 // 3. Funciones
 let resultado = sumar(2, 3);
 function sumar(num1, num2) {
-  return num1 + num2;
+    return num1 + num2;
 }
 
 console.log(resultado);
 
 let restar = function (num1, num2) {
-  return num1 - num2;
+    return num1 - num2;
 };
 console.log(restar(2, 1));
 
 // 3.1 Arrow functions
 let multiplicar = (num1, num2) => {
-  return num1 * num2;
+    return num1 * num2;
 };
 console.log(multiplicar(2, 6));
 
@@ -52,11 +52,11 @@ let division = (num1, num2) => num1 / num2;
 console.log(division(6, 3));
 
 let getUser = () => ({
-  name: "Andres",
+    name: 'Andres',
 });
 console.log(getUser());
 // 3. Arreglos
-let userPermissions = ["read", "update", "delete", "delete"];
+let userPermissions = ['read', 'update', 'delete', 'delete'];
 console.log(userPermissions[userPermissions.length - 1]);
 console.log(userPermissions.length);
 
@@ -70,23 +70,23 @@ console.log(newPermissions);
 console.log(newUserPermissions, userPermissions);
 
 let nine = 9;
-let nineString = "9";
+let nineString = '9';
 // Operador estricto
 console.log(nine === nineString);
 
 let filteredPermissions = userPermissions.filter(
-  (item) => item === "read" || item == "delete"
+    (item) => item === 'read' || item == 'delete'
 );
 console.log(filteredPermissions, userPermissions);
 
-let permissionString = userPermissions.join(",");
+let permissionString = userPermissions.join(',');
 console.log(permissionString);
 
 let numberArray = [1, 2, 3, 5];
 let numberSum = numberArray.reduce((item, sum) => sum + item);
 console.log(numberSum);
 
-const deleteIndex = userPermissions.findIndex((item) => item === "delete");
+const deleteIndex = userPermissions.findIndex((item) => item === 'delete');
 console.log(deleteIndex);
 
 const greaterThanZero = numberArray.every((item) => item > 0);
@@ -106,22 +106,26 @@ const no3Array = numberArray.filter((item) => item !== 3);
 console.log(no3Array);
 
 const added4ToArray = [...numberArray];
-added4ToArray.splice(numberArray.findIndex(item => item > 3), 0, 4);
+added4ToArray.splice(
+    numberArray.findIndex((item) => item > 3),
+    0,
+    4
+);
 console.log(added4ToArray, numberArray);
 
-const slicedArray = numberArray.slice(0,2);
+const slicedArray = numberArray.slice(0, 2);
 console.log(slicedArray);
 
 // 5. Objetos
 const userProps = {
-  name: 'Andres',
-  lastName: 'Rubio',
-  type: 'admin',
-  age: 30
-}
+    name: 'Andres',
+    lastName: 'Rubio',
+    type: 'admin',
+    age: 30,
+};
 const userProduct = {
-  productName: 'tictactoe',
-  productRole: 'developer'
+    productName: 'tictactoe',
+    productRole: 'developer',
 };
 
 // Object.freeze(userProps);
@@ -133,7 +137,7 @@ const newObject = Object.assign(userProps);
 console.log(newObject);
 
 // 5.1 Destructuring
-const newerObject = {...newObject, ...userProduct};
+const newerObject = { ...newObject, ...userProduct };
 console.log(newerObject);
 
 const { name: userName2, lastName: lastName2 } = userProps;
@@ -141,13 +145,13 @@ console.log(userName2, lastName2);
 
 // 5.2 Object Literal Enhancement
 const otherObject = {
-  userName2,
-  lastName2
-}
+    userName2,
+    lastName2,
+};
 console.log(otherObject);
 
 // 5.3 Manipulación de objetos (delete)
-delete userProps.type
+delete userProps.type;
 console.log(userProps);
 
 // 6 Operadores ternarios
--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,38 +1,160 @@
-const userNameInput = document.querySelector("#userName");
-const continueButton = document.querySelector("#continue-button");
+import { updateArrayItemByPropertyName } from './functions.js';
 
-continueButton.setAttribute("disabled", true);
+const userNameInput = document.querySelector('#userName');
+const continueButton = document.querySelector('#continue-button');
+const symbolInput = document.querySelector('#symbol');
+const formTitle = document.querySelector('.form-title');
+const form = document.querySelector('#form');
+let registryStep = 1;
+let players = [
+    {
+        id: 1,
+        name: 'Player 1',
+        symbol: 'circle',
+        hasBeenSet: false,
+    },
+    {
+        id: 2,
+        name: 'Player 2',
+        symbol: 'cross',
+        hasBeenSet: false,
+    },
+];
 
-userNameInput.addEventListener("input", () => {
-  const inputErrorMessage = userNameInput.parentElement.querySelector(
-    ".input-error-message"
-  );
-  const inputValue = userNameInput.value.trim(); // Remove trailing or leading spaces
-  const inputError =
-    inputValue.length === 0
-      ? { error: "required" }
-      : inputValue.length < 4
-      ? { error: "minlength" }
-      : null;
-
-  if (inputError) {
-    userNameInput.classList.add("input--invalid");
-    userNameInput.classList.remove("input--success");
-    continueButton.setAttribute("disabled", true);
-    if (inputError.error === "required") {
-      inputErrorMessage.classList.add("show");
-      inputErrorMessage.innerHTML = "User name is required";
-    }
-    if (inputError.error === "minlength") {
-      inputErrorMessage.innerHTML =
-        "User name should be at least 4 character long";
-    }
-    inputErrorMessage.classList.add("show");
-  } else {
-    userNameInput.classList.remove("input--invalid");
-    userNameInput.classList.add("input--success");
-    continueButton.removeAttribute("disabled");
-    inputErrorMessage.classList.remove("show");
-    inputErrorMessage.innerHTML = "";
-  }
+// User name input validation
+userNameInput.addEventListener('input', () => {
+    checkUserNameInputForErrors();
 });
+
+// Symbol input validation
+symbolInput.addEventListener('change', () => {
+    checkSymbolInputForDuplicateSelection();
+});
+
+// Checks form for changes and sets current player hasBeenSet property
+form.addEventListener('change', () => {
+    let currentPlayer = players.find((item) => item.id === registryStep);
+
+    currentPlayer = {
+        ...currentPlayer,
+        hasBeenSet: true
+    }
+
+    players = updateArrayItemByPropertyName(players, currentPlayer);
+})
+
+// Set player data on continue click
+continueButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let player = players.find((item) => item.id === registryStep);
+
+    player = {
+        ...player,
+        name: userNameInput.value,
+        symbol: symbolInput.value,
+    };
+    players = updateArrayItemByPropertyName(players, player);
+    registryStep++;
+    prepareFormForNextPlayer();
+    checkSymbolInputForDuplicateSelection();
+});
+
+
+/**
+ * Sets form default values for next player
+ */
+function prepareFormForNextPlayer() {
+    const nextPlayer = players.find((item) => item.id === registryStep);
+
+    userNameInput.value = '';
+    userNameInput.classList.remove('input--success');
+    symbolInput.classList.remove('input--success');
+    formTitle.innerHTML = nextPlayer.name;
+    symbolInput.value = nextPlayer.symbol;
+}
+
+/**
+ * Checks symbol input for duplicate values
+ */
+function checkSymbolInputForDuplicateSelection() {
+    const currentPlayer = players.find((item) => item.id === registryStep);
+
+    if (currentPlayer.hasBeenSet) {
+        const symbolValue = symbolInput.value;
+        const symbolInputParent = symbolInput.parentElement.parentElement;
+        const isSymbolAlreadySelected = players
+            .filter((item) => item.hasBeenSet)
+            .some(
+                (item) =>
+                    item.id !== currentPlayer.id && item.symbol === symbolValue
+            );
+
+        if (isSymbolAlreadySelected) {
+            const inputErrorElement = document.createElement('p');
+
+            inputErrorElement.classList.add('input-error-message', 'show');
+            symbolInput.classList.add('input--invalid');
+            symbolInput.classList.remove('input--success');
+            continueButton.setAttribute('disabled', true);
+            inputErrorElement.innerHTML = 'This value is already selected';
+            symbolInputParent.appendChild(inputErrorElement);
+        } else {
+            const inputErrorMessage = symbolInputParent.querySelector(
+                '.input-error-message'
+            );
+
+            symbolInput.classList.remove('input--invalid');
+            symbolInput.classList.add('input--success');
+            continueButton.removeAttribute('disabled');
+            if (inputErrorMessage) {
+                inputErrorMessage.remove();
+            }
+        }
+        checkUserNameInputForErrors();
+    }
+}
+
+function checkUserNameInputForErrors() {
+    const inputErrorMessage = userNameInput.parentElement.querySelector(
+        '.input-error-message'
+    );
+    const inputValue = userNameInput.value.trim(); // Remove trailing or leading spaces
+    const currentPlayer = players.find((item) => item.id === registryStep);
+    const isNameDuplicated = players
+        .filter((item) => item.hasBeenSet)
+        .some(
+            (item) => item.id !== currentPlayer.id && item.name === inputValue
+        );
+    const inputError =
+        inputValue.length === 0
+            ? { error: 'required' }
+            : inputValue.length < 4
+            ? { error: 'minlength' }
+            : isNameDuplicated
+            ? { error: 'duplicated' }
+            : null;
+
+    formTitle.innerHTML = inputValue;
+    if (inputError) {
+        userNameInput.classList.add('input--invalid');
+        userNameInput.classList.remove('input--success');
+        continueButton.setAttribute('disabled', true);
+        if (inputError.error === 'required') {
+            inputErrorMessage.innerHTML = 'User name is required';
+        }
+        if (inputError.error === 'minlength') {
+            inputErrorMessage.innerHTML =
+                'User name should be at least 4 character long';
+        }
+        if (inputError.error === 'duplicated') {
+            inputErrorMessage.innerHTML = 'This user name already exists';
+        }
+        inputErrorMessage.classList.add('show');
+    } else {
+        userNameInput.classList.remove('input--invalid');
+        userNameInput.classList.add('input--success');
+        continueButton.removeAttribute('disabled');
+        inputErrorMessage.classList.remove('show');
+        inputErrorMessage.innerHTML = '';
+    }
+}
